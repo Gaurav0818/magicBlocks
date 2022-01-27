4272a56bd1718034907c6bb8656dcabf583a885e
Gaurav0818 <80704349+Gaurav0818@users.noreply.github.com>
2022-01-27 21:05:37 +0530

    basic movement

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,10 @@ public class PlayerMovement : MonoBehaviour
     public float checkRadius;
     public LayerMask WhatisGround;
 
+
+
+    private bool rightDir = true; 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -29,10 +34,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WhatisGround);
+        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WhatisGround);
 
         moveInput = Input.GetAxis("Horizontal");
+
+        if((rightDir && moveInput < 0) || (!rightDir && moveInput>0))
+            rotate();
+
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
+    private void rotate()
+    {
+        rightDir = !rightDir;
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+    }
 }
